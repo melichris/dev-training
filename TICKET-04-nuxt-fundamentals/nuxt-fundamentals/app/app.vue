7fb897b91e8a0f199d0b677e859e27b5cd404524
Melichris <melichristian51@gmail.com>
2026-09-07 14:43:02 +0100

    feat: add navigation and page components for Nuxt fundamentals app

--- TICKET-04-nuxt-fundamentals/nuxt-fundamentals/app/app.vue
+++ TICKET-04-nuxt-fundamentals/nuxt-fundamentals/app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/posts">Posts</NuxtLink>
+      <NuxtLink to="/posts/1">Post 1</NuxtLink>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
