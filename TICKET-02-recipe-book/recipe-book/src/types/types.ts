6b1f3e4a2b22c3cfb5be9419bb3fa1a8a3088afd
Melichris <melichristian51@gmail.com>
2026-09-14 10:57:15 +0100

    fix: update createdAt timestamps in mockRecipes for accurate data representation

--- TICKET-02-recipe-book/recipe-book/src/types/types.ts
+++ TICKET-02-recipe-book/recipe-book/src/types/types.ts
@@ -8,27 +8,30 @@ export interface Recipe {
 export type Status = 'loading' | 'success' | 'error'
 export type SortBy = 'name' | 'createdAt' | 'vegetarian'
 export type NewRecipe = Omit<Recipe, 'id'>
+const ONE_HOUR = 1000 * 60 * 60
+const ONE_DAY = ONE_HOUR * 24
+const TWO_DAYS = ONE_DAY * 2
 export const mockRecipes: Record<number, Recipe> = {
   1: {
     id: 1,
     name: 'Recipe 1',
     ingredients: ['cube', 'salt', 'water'],
     isVegetarian: true,
-    createdAt: Date.now(),
+    createdAt: Date.now() - ONE_DAY,
   },
   2: {
     id: 2,
     name: 'Recipe 2',
     ingredients: ['palm oil', 'salt', 'water'],
     isVegetarian: false,
-    createdAt: Date.now(),
+    createdAt: Date.now() - TWO_DAYS,
   },
   3: {
     id: 3,
     name: 'Recipe 3',
     ingredients: ['corn', 'flour', 'water'],
     isVegetarian: true,
-    createdAt: Date.now(),
+    createdAt: Date.now() - TWO_DAYS,
   },
   4: {
     id: 4,
