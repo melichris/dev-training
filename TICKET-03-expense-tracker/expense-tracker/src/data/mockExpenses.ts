e9aad6100d359ec8356b4f5f3aa8e4c09e705a91
Melichris <melichristian51@gmail.com>
2026-09-16 22:18:25 +0100

    feat: add mock expenses data for testing and development purposes

---
+++ TICKET-03-expense-tracker/expense-tracker/src/data/mockExpenses.ts
@@ -0,0 +1,35 @@
+import type { Expense } from '@/types/types'
+export const mockExpenses: Record<number, Expense> = {
+  1: {
+    id: 1,
+    amount: 50,
+    description: 'Groceries',
+    date: '2023-06-01',
+    category: 'food',
+    paid: true,
+  },
+  2: {
+    id: 2,
+    amount: 20,
+    description: 'Bus fare',
+    date: '2023-06-02',
+    category: 'transport',
+    paid: false,
+  },
+  3: {
+    id: 3,
+    amount: 100,
+    description: 'Electricity bill',
+    date: '2023-06-03',
+    category: 'bills',
+    paid: true,
+  },
+  4: {
+    id: 4,
+    amount: 30,
+    description: 'Movie tickets',
+    date: '2023-06-04',
+    category: 'others',
+    paid: false,
+  },
+}
